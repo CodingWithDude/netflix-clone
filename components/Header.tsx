24f002a078cf38ed0424add687f0374f2d9ee88d
CodingWithDude <codingwithdude@gmail.com>
2022-10-01 17:14:55 -0700

    Updated Header and added globals.css
    Added links and netflix svg to Header
    Added headerLink class to globals.css
    Added body styling in globals.css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
-
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="Netflix Logo"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div></div>
+    </header>
+  );
 };
 
 export default Header;
