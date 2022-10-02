2949bb0c180c93be3125d4c92d34d926df5a3d0b
CodingWithDude <codingwithdude@gmail.com>
2022-10-01 17:50:32 -0700

    Added isScrolled State to Header
    Added useEffect to track isScrolled State to style header bg color

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,20 @@
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header>
+    <header className={`${isScrolled} && "bg-[#141414]"`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
