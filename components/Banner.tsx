c68744cb468991d385551b1462d3fe7127d59425
CodingWithDude <codingwithdude@gmail.com>
2022-10-01 23:31:47 -0700

    Updated button styling in banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,7 +19,7 @@ const Banner = ({ netflixOriginals }: Props) => {
   }, [netflixOriginals]);
 
   return (
-    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+    <div className="flex flex-col space-y-2 space-x-3 lg:space-x-8 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
